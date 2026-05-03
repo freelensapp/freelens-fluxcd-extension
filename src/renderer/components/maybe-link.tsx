ddcf7e55661b94b20247795afbd189a7abca87bc
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2026-05-03 16:52:00 +0200

    Workaround for Vite 8 and externalized modules

--- src/renderer/components/maybe-link.tsx
+++ src/renderer/components/maybe-link.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import * as ReactRouterDom from "react-router-dom";
+
+const { Link } = ReactRouterDom;
 
 export function MaybeLink(props: React.ComponentProps<typeof Link>): React.ReactElement {
   const { children, to, ...rest } = props;
